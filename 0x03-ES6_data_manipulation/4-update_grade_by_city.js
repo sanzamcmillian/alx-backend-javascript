c9326027de1a9c5b50e28fa8815db8f9565ac632
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-02 14:00:30 +0200

    4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,17 @@
 export default function updateStudentGradeByCity(student, city, newGrades) {
-    
+  const defaultGrade = { grade: 'N/A' };
+
+  if (!(student instanceof Array)) {
+    return [];
+  }
+  return student
+    .filter((student) => student.location === city)
+    .map((student) => ({
+      id: student.id,
+      firstName: student.firstName,
+      location: student.location,
+      grade: (newGrades
+        .filter((grade) => grade.studentId === student.id)
+        .pop() || defaultGrade).grade,
+    }));
 }
