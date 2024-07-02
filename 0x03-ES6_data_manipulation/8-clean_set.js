f839fc5083bbd1001debd7ca88672cdd51554bb4
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-02 19:00:47 +0200

    8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+    const parts = [];
+    if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+        return '';
+    }
+    for (const value of set.values()) {
+        if (typeof value === 'string' && value.startsWith(startString)) {
+            const valueSubstr = 'string' && value.substring(startString.length);
+
+            if (valueSubstr && valueSubstr !== value) {
+                parts.push(valueSubstr);
+            }
+        }
+    }
+    return parts.join('-');
+}
