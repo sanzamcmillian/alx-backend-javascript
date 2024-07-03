f9e6b5ea432fb437d80c99f2b86228921a1cc22e
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-03 14:20:26 +0200

    3

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,16 @@
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [index:string]: any;
+}
+
+export interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+export default function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+}
